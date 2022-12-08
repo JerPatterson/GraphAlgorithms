e86db37588391ff0a776bbe3480925ec8e86b031
JerPatterson <jeremie.patterson@polymtl.ca>
2022-12-07 20:07:48 -0500

    no weight graph main

--- java/graph/Main.java
+++ java/graph/Main.java
@@ -10,6 +10,7 @@ public class Main {
         graph.addNewVertex(2, new Integer[] {1, 3, 4, 5, 7});
         graph.addNewVertex(3, new Integer[] {4});
         graph.addNewVertex(4, new Integer[] {1, 7, 2});
+        graph.addNewVertex(5, new Integer[] {3, 4});
         graph.addNewVertex(7, new Integer[] {1, 2, 6, 7});
 
         graph.print();
