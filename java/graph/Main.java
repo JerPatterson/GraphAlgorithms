708c18eaa73bf2e11bfa68693b396b2362b45815
JerPatterson <jeremie.patterson@polymtl.ca>
2022-12-08 12:48:50 -0500

    main implementation for no weight graphs 'tests'

--- java/graph/Main.java
+++ java/graph/Main.java
@@ -2,23 +2,36 @@ package graph;
 
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph();
+        NoWeightGraph graph0 = new NoWeightGraph();
+        graph0.addNewVertex(1, new Integer[] {4, 3});
+        graph0.addNewVertex(2, new Integer[] {1, 3, 4, 5, 7});
+        graph0.addNewVertex(3, new Integer[] {4});
+        graph0.addNewVertex(4, new Integer[] {1, 7, 2});
+        graph0.addNewVertex(5, new Integer[] {3, 4});
+        graph0.addNewVertex(6, new Integer[] {2});
+        graph0.addNewVertex(7, new Integer[] {1, 2, 6, 7});
+        graph0.addNewVertex(8, new Integer[] {9, 10});
+        graph0.addNewVertex(9, new Integer[] {8, 10});
+        graph0.addNewVertex(10, new Integer[] {});
 
-        graph.addNewVertex(1, new Integer[] {4, 3}, new Integer[] {24, 32});
-        graph.addNewVertex(2, new Integer[] {1, 3, 4, 5, 7}, new Integer[] {11, 31, 41, 52, 72});
-        graph.addNewVertex(3, new Integer[] {4}, new Integer[] {14});
-        graph.addNewVertex(4, new Integer[] {1, 7, 2}, new Integer[] {21, 27, 42});
-        graph.addNewVertex(5, new Integer[] {3, 4}, new Integer[] {3, 55});
-        graph.addNewVertex(6, new Integer[] {2}, new Integer[] {36});
-        graph.addNewVertex(7, new Integer[] {1, 2, 6, 7}, new Integer[] {17, 29, 69, 78});
-
-        graph.print();
+        System.out.println(graph0);
         System.out.println();
 
-        graph.prim().print();
+        System.out.println(graph0.bfs());
+        System.out.println(graph0.dfs());
+        System.out.println(graph0.getDfsClosingTimes("1"));
+        System.out.println(graph0.getTransposed().getDfsClosingTimes("1"));
+        System.out.println(graph0.getStronglyConnectedComponents());
         System.out.println();
 
-        graph.kruskal().print();
-        System.out.println();
+
+        Graph graph1 = new Graph();
+        graph1.addNewVertex(1, new Integer[] {4, 3}, new Integer[] {24, 32});
+        graph1.addNewVertex(2, new Integer[] {1, 3, 4, 5, 7}, new Integer[] {11, 31, 41, 52, 72});
+        graph1.addNewVertex(3, new Integer[] {4}, new Integer[] {14});
+        graph1.addNewVertex(4, new Integer[] {1, 7, 2}, new Integer[] {21, 27, 42});
+        graph1.addNewVertex(5, new Integer[] {3, 4}, new Integer[] {3, 55});
+        graph1.addNewVertex(6, new Integer[] {2}, new Integer[] {36});
+        graph1.addNewVertex(7, new Integer[] {1, 2, 6, 7}, new Integer[] {17, 29, 69, 78});
     }
 }
