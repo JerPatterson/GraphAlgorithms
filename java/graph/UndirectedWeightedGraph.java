bcd1212808accee2d859efe93a7017ee4cc31afa
JerPatterson <jeremie.patterson@polymtl.ca>
2022-12-08 21:41:21 -0500

    creates basic implementation of undirected but weighted graph

--- java/graph/UndirectedWeightedGraph.java
+++ java/graph/UndirectedWeightedGraph.java
@@ -1,5 +1,27 @@
 package graph;
 
-public class UnorientedAndWeightedGraph {
-    Integer unoriented = 1;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+public class UndirectedWeightedGraph extends WeightedGraph {
+    UndirectedWeightedGraph() {
+        super();
+    }
+
+
+    @Override
+    public void addNewVertex(String identifier, String[] adjacents, Integer[] weights) {
+        super.addNewVertex(identifier, adjacents, weights);
+        int i = 0;
+        for (String adjacent : adjacents) {
+            if (identifier.compareTo(adjacent) != 0)
+                super.addNewVertex(adjacent, new String[] { identifier }, new Integer[] { weights[i++] });
+        }
+    }
+
+
+    @Override
+    public UndirectedWeightedGraph getTransposed() {
+        return this;
+    }
 }
